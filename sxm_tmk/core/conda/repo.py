d7746a7e59ee1af3836e7f11fd0d33bab3c58dac
Jean-Sébastien Dieu <jdieu@salsify.com>
2022-10-11 16:24:55 +0200

    B2K-1210: fat(progress) - Track progress on long running task (RepoQuery)
    Add the ability to track progress of long running tasks with a Progress.Task
    The `sxm_tmk.core.conda.repo.RepoQuery` is the first class to offer this ability.

--- sxm_tmk/core/conda/repo.py
+++ sxm_tmk/core/conda/repo.py
@@ -6,6 +6,7 @@ from typing import Dict, List, Optional
 import ujson
 
 from sxm_tmk.core.conda.cache import CondaCache
+from sxm_tmk.core.out.terminal import Progress
 from sxm_tmk.core.types import Packages
 
 
@@ -15,12 +16,14 @@ class SearchStatus(enum.Enum):
     NOT_FOUND = "not_found"
 
 
-def search_mamba_for(pkg: str, cache: CondaCache):
+def search_mamba_for(pkg: str, cache: CondaCache, progress_task: Progress.Task):
     if pkg not in cache:
         try:
             data = subprocess.check_output(f"mamba search --json {pkg}".split())
         except subprocess.CalledProcessError:
             return SearchStatus.NOT_FOUND, pkg
+        finally:
+            progress_task.update(1)
 
         json_data = ujson.loads(data)
         if "error" in json_data:
@@ -29,6 +32,7 @@ def search_mamba_for(pkg: str, cache: CondaCache):
             cache.store(pkg, data.decode("utf8"))
         return SearchStatus.FOUND_IN_REPOSITORY, pkg
     else:
+        progress_task.update(1)
         return SearchStatus.FOUND_IN_CACHE, pkg
 
 
@@ -41,11 +45,11 @@ class QueryPlan:
     def _aggregate_results(self, search_result: SearchStatus, pkg: str):
         self.__stats[str(search_result.value)].append(pkg)
 
-    def search_and_mark(self, packages: Packages):
+    def search_and_mark(self, packages: Packages, progress_track: Progress.Task):
         futures = []
         with ThreadPoolExecutor(max_workers=10) as tp:
             for package in packages:
-                futures.append(tp.submit(search_mamba_for, package.name, self.__cache))
+                futures.append(tp.submit(search_mamba_for, package.name, self.__cache, progress_track))
             wait(futures, return_when=ALL_COMPLETED)
         for result in futures:
             k, v = result.result()
--- sxm_tmk/tests/conda/test_repo.py
+++ sxm_tmk/tests/conda/test_repo.py
@@ -6,13 +6,15 @@ import ujson
 from sxm_tmk.core.conda.cache import CondaCache
 from sxm_tmk.core.conda.repo import QueryPlan, SearchStatus, search_mamba_for
 from sxm_tmk.core.dependency import Package
+from sxm_tmk.core.out.terminal import Progress
 
 
 def test_search_mamba_package_issue(tmp_path):
     cache = CondaCache(tmp_path)
+    task = Progress("").add_task("mamba", 1)
     with mock.patch("sxm_tmk.core.conda.repo.subprocess.check_output") as mocked_search:
         mocked_search.side_effect = CalledProcessError(3, cmd="mamba search something")
-        result, pkg = search_mamba_for("something", cache)
+        result, pkg = search_mamba_for("something", cache, task)
         assert result == SearchStatus.NOT_FOUND
         assert pkg == "something"
     mocked_search.assert_called_once()
@@ -20,6 +22,8 @@ def test_search_mamba_package_issue(tmp_path):
 
 def test_search_mamba_package_not_found(tmp_path):
     cache = CondaCache(tmp_path)
+    task = Progress("").add_task("mamba", 1)
+
     with mock.patch("sxm_tmk.core.conda.repo.subprocess.check_output") as mocked_search:
         mocked_search.return_value = ujson.dumps(
             {
@@ -37,7 +41,7 @@ def test_search_mamba_package_not_found(tmp_path):
                 "packages_formatted": "  - attrs2",
             }
         )
-        result, pkg = search_mamba_for("something", cache)
+        result, pkg = search_mamba_for("something", cache, task)
         assert result == SearchStatus.NOT_FOUND
         assert pkg == "something"
     mocked_search.assert_called_once()
@@ -45,9 +49,10 @@ def test_search_mamba_package_not_found(tmp_path):
 
 def test_search_mamba_package_found_in_cache(tmp_path):
     cache = CondaCache(tmp_path)
+    task = Progress("").add_task("mamba", 1)
     cache.store("something", ujson.dumps({"some": "value", "value": 42}))
     with mock.patch("sxm_tmk.core.conda.repo.subprocess.check_output") as mocked_search:
-        result, pkg = search_mamba_for("something", cache)
+        result, pkg = search_mamba_for("something", cache, task)
         assert result == SearchStatus.FOUND_IN_CACHE
         assert pkg == "something"
     mocked_search.assert_not_called()
@@ -55,15 +60,17 @@ def test_search_mamba_package_found_in_cache(tmp_path):
 
 def test_search_mamba_package_found_in_forge(tmp_path):
     cache = CondaCache(tmp_path)
+    task = Progress("").add_task("mamba", 1)
+
     with mock.patch("sxm_tmk.core.conda.repo.subprocess.check_output") as mocked_search:
         mocked_search.return_value = ujson.dumps({"some": "value", "value": 42}).encode("utf8")
-        result, pkg = search_mamba_for("something", cache)
+        result, pkg = search_mamba_for("something", cache, task)
         assert result == SearchStatus.FOUND_IN_REPOSITORY
         assert pkg == "something"
     mocked_search.assert_called_once()
 
 
-def search_mamba_for_replacement(dep: str, cache: CondaCache):
+def search_mamba_for_replacement(dep: str, cache: CondaCache, task: Progress.Task):
     return_value = {
         "numpy": (SearchStatus.FOUND_IN_REPOSITORY, {"numpy": [{"version": "1.2.3"}]}),
         "pytest": (SearchStatus.FOUND_IN_CACHE, {"pytest": [{"version": "4.5.6"}]}),
@@ -73,12 +80,19 @@ def search_mamba_for_replacement(dep: str, cache: CondaCache):
 
     if status == SearchStatus.FOUND_IN_REPOSITORY:
         cache.store(dep, ujson.dumps(value))
+    task.update(1)
     return status, dep
 
 
 def test_query_plan_search_result(tmp_path):
     # This test checks the results collected through the pool of query to ensure the final aggregation is correct
     a_cache: CondaCache = CondaCache(tmp_path)
+    all_pkgs_to_search = [
+        Package("numpy", version="1.2.3", build_number=None, build=None),
+        Package("pytest", version="4.5.6", build_number=None, build=None),
+        Package("thingy", version="1.0.0", build_number=None, build=None),
+    ]
+    task = Progress("").add_task("mamba", len(all_pkgs_to_search))
 
     # We want one value from the pool
     a_cache.store("pytest", ujson.dumps({"pytest": [{"version": "4.5.6"}]}))
@@ -88,12 +102,6 @@ def test_query_plan_search_result(tmp_path):
         "sxm_tmk.core.conda.repo.search_mamba_for", side_effect=search_mamba_for_replacement
     ) as search_mock:
 
-        all_pkgs_to_search = [
-            Package("numpy", version="1.2.3", build_number=None, build=None),
-            Package("pytest", version="4.5.6", build_number=None, build=None),
-            Package("thingy", version="1.0.0", build_number=None, build=None),
-        ]
-        q.search_and_mark(all_pkgs_to_search)
+        q.search_and_mark(all_pkgs_to_search, task)
     assert search_mock.call_count == 3
-
     assert q.stats == {"not_found": ["thingy"], "found": ["numpy", "pytest"]}
