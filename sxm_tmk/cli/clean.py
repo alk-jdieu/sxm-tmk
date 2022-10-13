ac3ac759d1ddaef54f7d4cba6404d109d3f2d707
Jean-Sébastien Dieu <jdieu@salsify.com>
2022-10-13 10:21:24 +0200

    B2K-1210: feat(cli) - Allow to empty the cache

--- sxm_tmk/cli/clean.py
+++ sxm_tmk/cli/clean.py
@@ -12,6 +12,7 @@ def setup(subparser):
         default=CACHE_DIR,
         help=f"Path to a conda cache. Default is to use {CACHE_DIR.as_posix()}",
     )
+    clean_parser.add_argument("--aggressive", action="store_true", help="Remove all files in cache.")
     clean_parser.set_defaults(func=main)
 
 
@@ -19,14 +20,18 @@ def main(options):
     Terminal("rich")
     Terminal().info("Validating cache path...")
     if not options.conda_cache.exists():
-        Terminal().step_status("Invalid cache", False)
+        Terminal().step("Invalid cache (no such dir)", False)
         return 1
 
-    Terminal().step_status("Cache is healthy", True)
+    if not (options.conda_cache / "tmk.lock").exists():
+        Terminal().step("Invalid cache (no lock file)", False)
+        return 1
+    Terminal().step("Cache is healthy", True)
+
     cache = CondaCache(options.conda_cache)
     state = Status("Cleaning ...")
     with state:
-        res = cache.clean()
+        res = cache.clean(options.aggressive)
 
     Terminal().info(f"Files deleted: {res['deleted']}")
     Terminal().info(f"Space claimed: {res['space-claimed'] / 1024:.2f} KB")
