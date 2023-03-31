c70b41ddc2a97ada0cd974acffd46cdad44042f3
Jean-Sébastien Dieu <jdieu@salsify.com>
2023-03-31 09:52:29 +0200

    fix(B2K-1300): Better error message and cleaning
    Improve error message when proceeding to the installation. Now always clean the generated script.

--- sxm_tmk/cli/create.py
+++ sxm_tmk/cli/create.py
@@ -1,11 +1,12 @@
 import datetime
 import pathlib
-from typing import Dict, List, Optional, Tuple
+from typing import Callable, Dict, List, Optional
 
 import yaml
 from pydantic import BaseModel, Field
 
 from sxm_tmk.core.bash import BashScript, ExecutionDir
+from sxm_tmk.core.conda.commands import MambaEnv
 from sxm_tmk.core.out.terminal import Section, Terminal
 
 
@@ -28,9 +29,12 @@ class EnvironmentMaker:
         exec_dir: pathlib.Path
         fail_msg: str
         run_msg: str
-        script: List[str] = Field(min_length=1)
         step_msg: str
         step_name: str
+
+    class Step(StepInfo):
+        script: Optional[List[str]] = Field(min_length=1)
+        call: Optional[Callable[[str], bool]] = None
 
     def __init__(
         self,
@@ -43,28 +47,48 @@ class EnvironmentMaker:
         self.__specification_path = specification_file
         self.__debug = debug
         self.__keep_files = keep_files
-        self.__steps: Dict[str, EnvironmentMaker.StepInfo] = {}
+        self.__steps: Dict[str, EnvironmentMaker.Step] = {}
         self.__environment_name: str = ""
 
     @property
     def env_name(self) -> str:
         return self.__environment_name
 
-    def _run(self, step_id: str) -> Tuple[BashScript, bool]:
+    def _run_script(self, now: str, step: Step) -> bool:
+        script: BashScript = BashScript(
+            script=step.script,  # type: ignore
+            name=f"{now}_{step.step_name}_{self.env_name}.sh",
+            echo_statement=self.__debug,
+        )
+        rc = script.run() == 0
+        if not self.__keep_files:
+            script.unlink()
+        if not rc:
+            explain = Section()
+            Terminal().error("\n" + step.fail_msg)
+            self._dump_step(script, use_section=explain)
+            Terminal().info(f"Environment creation failed with rc [{script.return_code}]")
+        return rc  # noqa R504
+
+    def _run_callable(self, step: Step) -> bool:
+        rc = step.call(self.env_name)  # type: ignore
+        if not rc:
+            Terminal().error("\n" + step.fail_msg)
+        return rc
+
+    def _run(self, step_id: str) -> bool:
         now: str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        step_info: EnvironmentMaker.StepInfo = self.__steps[step_id]
+        step_info: EnvironmentMaker.Step = self.__steps[step_id]
 
         status = Terminal().new_status(step_info.run_msg)
         execution_dir = ExecutionDir(use_tempdir=False, force_dir=step_info.exec_dir)
-        script: BashScript = BashScript(
-            script=step_info.script, name=f"{now}_{step_info.step_name}_{self.env_name}.sh", echo_statement=self.__debug
-        )
         with status, execution_dir:
-            succeeded = script.run() == 0
-        if not self.__keep_files:
-            script.unlink()
+            if step_info.script is not None:
+                succeeded = self._run_script(now, step_info)
+            else:
+                succeeded = self._run_callable(step_info)
         Terminal().step(step_info.step_msg, succeeded)
-        return script, succeeded
+        return succeeded
 
     def _read_environment_name(self) -> bool:
         try:
@@ -72,22 +96,32 @@ class EnvironmentMaker:
                 y = yaml.load(f, yaml.SafeLoader)
             self.__environment_name = y["name"]
         except (FileNotFoundError, KeyError):
-            Terminal().error("Cannot read environment name")
+            Terminal().error(f"Cannot read environment name. Please check your {self.__specification_path.name} file")
             return False
         return True
 
     def _prepare_steps(self):
-        create_step = EnvironmentMaker.StepInfo(
+        check_step = EnvironmentMaker.Step(
             exec_dir=self.__specification_path.parent,
-            fail_msg="Environment creation failed with rc",
+            fail_msg=f"This environment ({self.env_name}) already exists. "
+            f"You must remove it prior to launch this command.",
+            run_msg="Checking if an environment already exists",
+            step_msg="Environment available",
+            step_name="check",
+            call=lambda env_name: MambaEnv().exists(env_name),
+        )
+        create_step = EnvironmentMaker.Step(
+            exec_dir=self.__specification_path.parent,
+            fail_msg="Cannot create environment. Try with --debug to get more info",
             run_msg=f"Installing your project into environment [{self.env_name}]",
             script=[f"mamba env create -f {self.__specification_path.as_posix()}"],
             step_msg=f"Environment created ({self.env_name})",
             step_name="create",
         )
-        install_step = EnvironmentMaker.StepInfo(
+        install_step = EnvironmentMaker.Step(
             exec_dir=self.__project_root_path,
-            fail_msg=f"Installing {self.__project_root_path.name} in environment {self.env_name} failed with rc",
+            fail_msg=f"Cannot install {self.__project_root_path.name} in environment {self.env_name}."
+            f" Try with --debug for more info",
             run_msg=f"Installing project {self.__project_root_path.name} into environment [{self.env_name}]",
             script=[
                 'conda_root=$(conda config --show root_prefix | sed "s|.*: ||")',
@@ -99,6 +133,7 @@ class EnvironmentMaker:
             step_msg="Installed in environment",
             step_name="install",
         )
+        self.__steps[check_step.step_name] = check_step
         self.__steps[create_step.step_name] = create_step
         self.__steps[install_step.step_name] = install_step
 
@@ -106,15 +141,21 @@ class EnvironmentMaker:
         if not self._read_environment_name():
             return 1
         self._prepare_steps()
-        for rc_fail, step, method in [(2, "create", self._create_environment), (3, "install", self._install_in_env)]:
+        for rc_fail, (step, method) in enumerate(
+            [
+                ("check", self._check_environment),
+                ("create", self._create_environment),
+                ("install", self._install_in_env),
+            ]
+        ):
             method()
-            script_used, success = self._run(step)
+            success = self._run(step)
             if not success:
-                explain = Section()
-                Terminal().info(f"Environment creation failed with rc [{script_used.return_code}]")
-                self._dump_step(script_used, use_section=explain)
-                return rc_fail
+                return rc_fail + 2
         return 0
+
+    def _check_environment(self):
+        Terminal().info(f"Checking that {self.env_name} can be created")
 
     def _create_environment(self):
         Terminal().info(f'Using specification "{self.__specification_path.as_posix()}"')
@@ -146,7 +187,7 @@ def main(options):
         project_path=options.project_path,
         specification_file=options.spec,
         debug=options.debug,
-        keep_files=options.debug,
+        keep_files=False,
     )
     result = env.create()
     if result == 0:
